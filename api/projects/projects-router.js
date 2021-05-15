d84ffaeea83da69039a506440f3457ed1a846215
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-14 22:40:44 -0400

    Delete project complete.

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -31,4 +31,31 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+  const projectToEdit = await Projects.get(id);
+
+  if (!req.body || Object.keys(req.body).length === 0) {
+    res.status(400).json({ message: "Project name and description required" });
+  } else if (projectToEdit === null) {
+    res.status(404).json({ messgae: "Invalid ID" });
+  } else {
+    const updatedProject = await Projects.update(id, changes);
+    console.log(updatedProject);
+    res.status(200).json(updatedProject);
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const confirmation = await Projects.remove(id);
+
+  if (confirmation === 0) {
+    res.status(404).json({ message: "Invalid ID" });
+  } else {
+    res.status(200).json({});
+  }
+});
+
 module.exports = router;
